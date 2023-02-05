af8d19d0adf916094fecde9918506519d12e0286
Nitish Puri <npuri1903@gmail.com>
2023-02-05 11:52:28 +0530

    Generate image by filling in pixels, show image to viewport.

--- VkRayTracer/src/WalnutApp.cpp
+++ VkRayTracer/src/WalnutApp.cpp
@@ -8,12 +8,46 @@ class ExampleLayer : public Walnut::Layer
 public:
 	virtual void OnUIRender() override
 	{
-		ImGui::Begin("Hello");
-		ImGui::Button("Button");
+		ImGui::Begin("Settings");
+		if (ImGui::Button("Render")) {
+			Render();
+		}
 		ImGui::End();
 
-		ImGui::ShowDemoWindow();
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("Viewport");
+		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
+
+		if (m_Image) {
+			ImGui::Image(m_Image->GetDescriptorSet(), { (float)(m_Image->GetWidth()), (float)(m_Image->GetHeight()) });
+		}
+
+		ImGui::End();
+		ImGui::PopStyleVar();
+
+
+		//ImGui::ShowDemoWindow();
 	}
+
+	void Render() {
+		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight()) {
+			m_Image = std::make_shared<Walnut::Image>(m_ViewportWidth, m_ViewportHeight, Walnut::ImageFormat::RGBA);
+			delete[] m_ImageData;
+			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
+		}
+
+		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++) {
+			m_ImageData[i] = 0xffff00ff;
+		}
+
+		m_Image->SetData(m_ImageData);
+	}
+
+private:
+	std::shared_ptr<Walnut::Image> m_Image;
+	uint32_t* m_ImageData = nullptr;
+	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
