7d51c71fc16b4e6ddcc0cb2a1bc9db30aec27c4c
Nitish Puri <npuri1903@gmail.com>
2023-02-05 12:01:36 +0530

    Show random noise.

--- VkRayTracer/src/WalnutApp.cpp
+++ VkRayTracer/src/WalnutApp.cpp
@@ -2,6 +2,8 @@
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
+#include "Walnut/Random.h"
+#include "Walnut//Timer.h"
 
 class ExampleLayer : public Walnut::Layer
 {
@@ -9,6 +11,7 @@ public:
 	virtual void OnUIRender() override
 	{
 		ImGui::Begin("Settings");
+		ImGui::Text("Last render: %.3fms", m_LastRenderTime);
 		if (ImGui::Button("Render")) {
 			Render();
 		}
@@ -26,11 +29,15 @@ public:
 		ImGui::End();
 		ImGui::PopStyleVar();
 
+		Render();
+
 
 		//ImGui::ShowDemoWindow();
 	}
 
 	void Render() {
+		Walnut::Timer timer;
+
 		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight()) {
 			m_Image = std::make_shared<Walnut::Image>(m_ViewportWidth, m_ViewportHeight, Walnut::ImageFormat::RGBA);
 			delete[] m_ImageData;
@@ -38,16 +45,21 @@ public:
 		}
 
 		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++) {
-			m_ImageData[i] = 0xffff00ff;
+			//m_ImageData[i] = 0xffff00ff;
+			m_ImageData[i] = Walnut::Random::UInt();
+			//m_ImageData[i] |= 0xff000000;
 		}
 
 		m_Image->SetData(m_ImageData);
+
+		m_LastRenderTime = timer.ElapsedMillis();
 	}
 
 private:
 	std::shared_ptr<Walnut::Image> m_Image;
 	uint32_t* m_ImageData = nullptr;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
+	float m_LastRenderTime = 0.0f;
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
